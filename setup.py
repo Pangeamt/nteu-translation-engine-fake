0617a06b9be0a3e77261ef4bdd616744c8881cdf
Aleix <333aleix333@gmail.com>
2020-04-07 16:10:18 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nteu-engine-fake',
+    version='0.1',
+    description='NTEU engine',
+    url='https://github.com/Pangeamt/nteu-translation-engine-fake',
+    author='PangeaMT',
+    author_email='a.cerda@pangeanic.es',
+    license='MIT',
+    packages=['nteu_translation_engine_fake'],
+    zip_safe=False
+)
